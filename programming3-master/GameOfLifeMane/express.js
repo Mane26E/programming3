e174ccae9de6781a823191e95445b9ad1011dfd1
user <lusine.sargsyan1@tumo.org>
2023-12-04 21:15:47 +0400

    modul

---
+++ programming3-master/GameOfLifeMane/express.js
@@ -0,0 +1,17 @@
+// var app = express();
+
+// app.use(express.static("."));
+
+// app.get("/", function(req, res){
+// res.redirect("index.html");
+// });
+
+// app.listen(3000, function(){
+// console.log("Example is running on port 3000");
+// });
+
+
+let Square = require("./square")
+
+let obj = new Square(4)
+console.log(obj.tviQarakusi());
