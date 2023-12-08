0b676bb1f43efac45367c6d547c3534c6513c749
Mane26E <152201846+Mane26E@users.noreply.github.com>
2023-12-08 18:13:37 +0400

    Add files via upload

--- programming3-master/GameOfLifeMane/express.js
+++ programming3-master/GameOfLifeMane/express.js
@@ -1,17 +1,27 @@
-// var app = express();
-
-// app.use(express.static("."));
+var express = require("express");
+var app = express();
 
 // app.get("/", function(req, res){
 // res.redirect("index.html");
 // });
 
-// app.listen(3000, function(){
-// console.log("Example is running on port 3000");
+app.listen(3000, function() {
+    console.log("Example is running on port 3000");
+});
+
+
+// let Square = require("./square")
+
+// let obj = new Square(4)
+// console.log(obj.tviQarakusi());
+
+// app.get("/", function(req, res) {
+//     res.redirect("http://google.com");
 // });
 
+app.get("/", function(req, res, search) {
 
-let Square = require("./square")
+    search = "Anna Asti"
 
-let obj = new Square(4)
-console.log(obj.tviQarakusi());
+    res.redirect("http://google.com/search?q=" + search);
+});
